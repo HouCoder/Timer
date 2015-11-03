6dece6b67094296835cba28ef9270c13457942ab
Tonni <hby3387@gmail.com>
2015-11-03 22:53:06 +0800

    Fix incorrect path

--- js/config.js
+++ js/config.js
@@ -1,17 +1,21 @@
-requirejs.config({
-    baseUrl: './',
-    shim: {
-        'bootstrap': {
-            deps: [
-                'jquery'
-            ]
+(function (){
+    var COMPONENTS_PATH = '../bower_components/';
+
+    requirejs.config({
+        baseUrl: './js/',
+        shim: {
+            'bootstrap': {
+                deps: [
+                    'jquery'
+                ]
+            }
+        },
+        paths: {
+            jquery: COMPONENTS_PATH + 'jquery/dist/jquery.min',
+            uaparser: COMPONENTS_PATH + 'ua-parser-js/dist/ua-parser.min',
+            bootstrap: COMPONENTS_PATH + 'bootstrap/dist/js/bootstrap.min',
+            select2: COMPONENTS_PATH + 'select2/dist/js/select2.min',
+            howler: COMPONENTS_PATH + 'howler.js/howler.min'
         }
-    },
-    paths: {
-        jquery: window.TIMER.COMPONENTS_PATH + 'jquery/dist/jquery.min',
-        uaparser: window.TIMER.COMPONENTS_PATH + 'ua-parser-js/dist/ua-parser.min',
-        bootstrap: window.TIMER.COMPONENTS_PATH + 'bootstrap/dist/js/bootstrap.min',
-        select2: window.TIMER.COMPONENTS_PATH + 'select2/dist/js/select2.min',
-        howler: window.TIMER.COMPONENTS_PATH + 'howler.js/howler.min'
-    }
-});
+    });
+})();
